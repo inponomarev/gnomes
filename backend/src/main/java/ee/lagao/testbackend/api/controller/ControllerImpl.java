969da91b4e8c7e8ec23b4029294dd1011fe27c12
Ivan Ponomarev <iponomarev@mail.ru>
2021-06-23 13:34:27 +0300

    jwt-based security

--- backend/src/main/java/ee/lagao/testbackend/api/controller/ControllerImpl.java
+++ backend/src/main/java/ee/lagao/testbackend/api/controller/ControllerImpl.java
@@ -3,8 +3,13 @@ package ee.lagao.testbackend.api.controller;
 import ee.lagao.testbackend.api.dto.LoginDTO;
 import ee.lagao.testbackend.api.dto.LoginResultDTO;
 import ee.lagao.testbackend.api.dto.SecretPlanDTO;
+import ee.lagao.testbackend.security.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,24 +21,27 @@ import java.util.List;
 @CrossOrigin
 public class ControllerImpl implements Controller {
 
+    @Autowired
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    private final JwtUtils jwtUtils;
+
     @Override
     public LoginResultDTO login(LoginDTO request, HttpServletResponse response) {
-        LoginResultDTO result = new LoginResultDTO();
-        if ("admin".equals(request.getLogin())
-                && "admin".equals(request.getPassword())) {
-            result.setSessionId("dead-beef");
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        }
-        System.out.printf("Login: %s, Password: %s, Status: %d, SessionId: %s%n",
-                request.getLogin(),
-                request.getPassword(),
-                response.getStatus(),
-                result.getSessionId());
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getLogin(), request.getPassword()));
 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(request.getLogin());
+
+        var result = new LoginResultDTO();
+        result.setToken(jwt);
+        result.setType("Bearer");
+        result.setUsername(request.getLogin());
         return result;
     }
+
 
     @Override
     public SecretPlanDTO secretplan(HttpServletResponse response) {
---
+++ backend/src/main/java/ee/lagao/testbackend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,58 @@
+package ee.lagao.testbackend.security.services;
+
+import ee.lagao.testbackend.api.dto.LoginDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.Collections.emptyList;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+    private final Map<String, LoginDTO> users = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    void init() {
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setLogin("admin");
+        loginDTO.setPassword("admin");
+        /*UserDetails userDetails =
+                User.builder()
+                        .passwordEncoder(passwordEncoder::encode)
+                        .username("admin")
+                        .password("admin")
+                        .authorities(emptyList())
+                        .build();
+
+         */
+        users.put(loginDTO.getLogin(), loginDTO);
+    }
+
+    //TODO put a user to this map
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LoginDTO loginDTO = users.get(username);
+        if (loginDTO == null) {
+            throw new UsernameNotFoundException("User Not Found with username: " + username);
+        }
+        return User.builder().passwordEncoder(passwordEncoder::encode)
+                .username(loginDTO.getLogin())
+                .password(loginDTO.getPassword())
+                .authorities(emptyList())
+                .build();
+    }
+
+}
